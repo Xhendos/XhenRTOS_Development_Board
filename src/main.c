59df7307819773fc353b682d1f329b515c35eece
Xhendos <youri_business@outlook.com>
2020-06-27 01:14:44 +0200

    Added first code to set clocks to 72 MHz

--- src/main.c
+++ src/main.c
@@ -2,6 +2,22 @@
 
 int main()
 {
+    RCC->CR |= RCC_CR_HSEON;            /* Turn on the external high speed oscillator clock source */
+    while(!(RCC->CR & RCC_CR_HSERDY));  /* Wait untill the external clock source is stable */
+
+    RCC->CFGR &= ~(RCC_CFGR_HPRE);      /* Do not divide the clock source for AHB */
+    RCC->CFGR |= RCC_CFGR_PPRE1_2 |     /* Divide APB1 and APB2 by 2 (36 MHz is maximum) */
+                 RCC_CFGR_PPRE2_2 |    
+                 RCC_CFGR_PLLSRC |      /* HSE is the input clock source for the PLL */
+                 RCC_CFGR_PLLMULL_2;    /* Multiply the clock source by 6 (12 MHz * 6 = 72 MHz) */
+
+                                        /* Ratio system clock and flash access 
+                                         * should be 2 wait states for 48 < sysclk ≤ 72 */
+    FLASH->ACR &= ~(FLASH_ACR_LATENCY_0);   
+    
+    RCC->CR |= RCC_CR_PLLON;    
+    while(!(RCC->CR & RCC_CR_PLLRDY));  /* Wait untill the PLL is locked (which means ready) */
+                                        /* We should have a 72 MHz clock speed at this point */
 
 	return 0;
 }
